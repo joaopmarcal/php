a4078f92794a2c3e44eff63d21af364f1d731fe1
Unknown <paulospc21@gmail.com>
2018-11-23 16:19:46 -0300

    informações sobre interface

--- POO/interface.php
+++ POO/interface.php
@@ -1,5 +1,22 @@
 ﻿<?php
 
+interface Veiculo {
+	public function acelerar($velocidade);
+	public function freiar($velocidade);
+	public function trocarMarcha($marcha);
+}
+class Civic implements Veiculo {
+	public function acelerar($velocidade){
+		echo "O veiculo acelerou ".$velocidade . " KM/h";
+	}
+	public function freiar($velocidade){
+		echo "O veiculo frenou até  ".$velocidade . " KM/h";
+	}
+	public function trocarMarcha($marcha){
+		echo "O veiculo engatou a marcha ".$marcha;
+	}
+}
 
-
+$carro = new Civic();
+$carro->trocarMarcha(1);
 ?>
