27a7763ceb12e60740a11718baa931d62c6b3b80
Unknown <paulospc21@gmail.com>
2018-11-24 08:08:02 -0300

    polimorfismo

---
+++ POO/abstrata.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+interface Veiculo {
+	public function acelerar($velocidade);
+	public function freiar($velocidade);
+	public function trocarMarcha($marcha);
+}
+abstract class Automovel implements Veiculo {
+	public function acelerar($velocidade){
+		echo "O veiculo acelerou ".$velocidade . " KM/h";
+	}
+	public function freiar($velocidade){
+		echo "O veiculo frenou até  ".$velocidade . " KM/h";
+	}
+	public function trocarMarcha($marcha){
+		echo "O veiculo engatou a marcha ".$marcha;
+	}
+}
+
+
+class DelRey extends Automovel {
+	public function empurrar(){
+		
+	}
+}
+$carro = new DelRey();
+$carro->acelerar(200);
+?>
---
+++ POO/polimorfismo.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+abstract class Animal {
+	public function falar(){
+		return "som";
+	}
+	public function mover(){
+		return "Anda";
+	}	
+}
+class Cachorro extends Animal {
+	public function falar(){
+		return "late";
+	}
+}
+class Gato extends Animal {
+	public function falar(){
+		return "Mia";
+	}
+}
+class Passaro extends Animal {
+	public function falar(){
+		return "Canta";
+	}
+	public function mover(){
+		return "Voa e " . parent::mover();
+	}	
+}
+$pluto = new Cachorro();
+echo $pluto->falar() . "<br>";
+echo $pluto->mover() . "<br>";
+$gatinho = new Gato();
+echo $gatinho->falar() . "<br>";
+echo "-----------------------<br>";
+$passarinho = new Passaro();
+echo $passarinho->falar() . "<br>";
+echo $passarinho->mover() . "<br>";
+?>
