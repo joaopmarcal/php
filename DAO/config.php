94b34dc8d82cb61f83c48d61bade2c3f353e6e6a
João Paulo <paulospc21@gmail.com>
2018-11-26 15:54:28 -0300

    incluída classe usuario

--- DAO/config.php
+++ DAO/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	spl_autoload_register(function($class_name){
-		$filename = "class".DIRECTORY_SEPARATOR.$class_name. ".php";
+		$filename = "class".DIRECTORY_SEPARATOR.$class_name. ".php";//class é o nome da pasta que estão as classes.
 		if(file_exists(($filename))){
 			require_once($filename);
 		}
