bd6fb26f530f555bdcca50cd609a4d30257b8297
João Paulo <paulospc21@gmail.com>
2018-11-26 16:53:39 -0300

    criado novas métodos para usuários

--- DAO/index.php
+++ DAO/index.php
@@ -1,10 +1,21 @@
 <?php
 
 require_once("config.php");
+//carrega um usuário apenas
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
 
-$root = new Usuario();
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadbyId(4);
-echo $root;
+// Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//carrega um usuário
+$usuario = new Usuario();
+$usuario->login("joao","9875");
+echo $usuario;
 ?>
